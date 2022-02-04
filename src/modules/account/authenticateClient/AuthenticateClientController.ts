6cb1f201fd5466c7e693d8d5de761f5aba02c3a7
bfsantos <bfsantos@rd.com.br>
2022-02-04 07:41:08 -0300

    tratamento de erros

--- src/modules/account/authenticateClient/AuthenticateClientController.ts
+++ src/modules/account/authenticateClient/AuthenticateClientController.ts
@@ -3,13 +3,9 @@ import { AuthenticateClientUseCase } from "./AuthenticateClientUseCase";
 
 export class AuthenticateClientController {
     async handle(req: Request, res: Response) {
-        try {
             const { username, password } = req.body; 
             const authenticateClient = new AuthenticateClientUseCase();
             const auth = await authenticateClient.execute({ username, password });
             return res.json(auth);
-        } catch (error) {
-            return res.json({ message: error });
-        }
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import "dotenv/config";
 import { routes } from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            message: err.message
+        });
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+    next()
+})
 app.listen(2022, () => console.log("Server is running"));
