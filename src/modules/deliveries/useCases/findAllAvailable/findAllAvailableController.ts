7677e469b50af7836d5dfc8e858f3501c0dee5f9
Bia-source <bia_ferreirads@yahoo.com>
2022-02-16 03:33:09 -0300

    ajustando find all client and deliveryman

--- src/modules/deliveries/useCases/findAllAvailable/findAllAvailableController.ts
+++ src/modules/deliveries/useCases/findAllAvailable/findAllAvailableController.ts
@@ -5,8 +5,9 @@ import { FindAllAvailableUseCase } from "./FindAllAvailableUseCase";
 export class FindAllAvailableController {
     async handle(req: Request, res: Response): Promise<Response> {
         try {
+            const id_user = req.id;
             const deliveriesAllUseCase = new FindAllAvailableUseCase();
-            const result = await deliveriesAllUseCase.execute();
+            const result = await deliveriesAllUseCase.execute(id_user);
             return res.json({ deliveries: result });
         } catch (error) {
             return res.json({ message: error.message });
--- src/modules/deliveries/useCases/findAllAvailable/findAllAvailableUseCase.ts
+++ src/modules/deliveries/useCases/findAllAvailable/findAllAvailableUseCase.ts
@@ -3,24 +3,45 @@ import { mapDelivery } from "../../../../share/FormatReturn/map";
 
 
 export class FindAllAvailableUseCase {
-    async execute() {
-        const deliveries = await prisma.deliveries.findMany({
-            where: {
-                end_at: null,
-                id_deliveryman: null
-            },
-            select: {
-                id: true,
-                client: true,
-                item_name: true,
-                id_deliveryman: true,
-                created_at: true,
-                end_at: true,
-                status: true,
+    async execute(id_user: string) {
+            const deliveriesClient = await prisma.deliveries.findMany({
+                where: {
+                    end_at: null,
+                    id_deliveryman: null,
+                    id_client: id_user
+                },
+                select: {
+                    id: true,
+                    client: true,
+                    item_name: true,
+                    id_deliveryman: true,
+                    created_at: true,
+                    end_at: true,
+                    status: true,
+                }
+            });
+
+            const deliveriesDeliveryman = await prisma.deliveries.findMany({
+                where: {
+                    end_at: null,
+                    id_deliveryman: null
+                },
+                select: {
+                    id: true,
+                    client: true,
+                    item_name: true,
+                    id_deliveryman: true,
+                    created_at: true,
+                    end_at: true,
+                    status: true,
+                }
+            });
+            if(deliveriesClient.length === 0){
+               return mapDelivery(deliveriesDeliveryman);
             }
-        });
-        
-        return mapDelivery(deliveries);
+            
+            return  mapDelivery(deliveriesClient) 
+       
+        }
     }
-}
 
