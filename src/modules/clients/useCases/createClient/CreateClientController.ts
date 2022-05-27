270967fd71b007102a015dab389804ca1643ef09
Bia-source <bia_ferreirads@yahoo.com>
2022-05-26 23:44:50 -0300

    add cors

--- src/modules/clients/useCases/createClient/CreateClientController.ts
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -4,7 +4,9 @@ import { CreateClientUseCase } from "./CreateClientUseCase";
 export class CreateClientController {
     async handle(req: Request, res: Response) {
         try {
+           
             const { username, password, email } = req.body;
+            console.log(username, password, email)
             const createClientUseCase = new CreateClientUseCase();
             const result = await createClientUseCase.execute({ username, password, email });
             return res.json({client: result});
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import "dotenv/config";
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 import { routes } from './index.routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/delivery-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
@@ -24,3 +26,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     
 })
 app.listen(2022, () => console.log("Server is running"));
+
