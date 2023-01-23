8072985b3bd2005127b6673ffbff1ea804251b4f
Bia-source <bia_ferreirads@yahoo.com>
2023-01-22 23:07:44 -0300

    ajustando use case deletar client com pedidos feitos

--- src/modules/clients/useCases/deleteClient/DeleteClientUseCase.ts
+++ src/modules/clients/useCases/deleteClient/DeleteClientUseCase.ts
@@ -1,8 +1,13 @@
 import { prisma } from "../../../../database/prismaClient";
 
 export class DeleteClientUseCase {
-    async execute(id_client: string){
-        const client = await prisma.clients.delete({where: { id: id_client}});
-        return client;
+    async execute(id_client: string) {
+        try {
+            await prisma.deliveries.deleteMany({ where: { id_client: id_client } })
+            const client = await prisma.clients.delete({ where: { id: id_client } });
+            return client;
+        } catch (error) {
+             return error;
+        }
     }
 }
